073b1d48e92be0a4f4464ba964f6967034ac3c9e
qihangg <122329456+qihangg@users.noreply.github.com>
2023-02-27 18:43:33 +0800

    Timer created by qihang

---
+++ Assets/scripts/Timerscript.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class Timerscript : MonoBehaviour
+{
+    public float timeleft;
+
+    public int timeRemaining;
+
+    public Text  TimerText;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timeleft -= Time.deltaTime;
+
+        timeRemaining = Mathf.FloorToInt(timeleft % 60);
+
+        TimerText.text = "Timer : " + timeRemaining.ToString();
+
+        if(timeRemaining <= 0)
+        {
+            SceneManager.LoadScene("LoseScene");
+        }
+    }
+}
